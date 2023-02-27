75baf77e18c84d6be626ea5213b2838391ce0b5b
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-28 01:09:15 +0700

    config cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,5 +1,6 @@
 export const corsOptions = {
-  origin: "*",
+  origin: "https://simple-project-123.netlify.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowHeaders: ["Content-Type", "multipart/form-data", ""],
+  credentials: true,
 };
